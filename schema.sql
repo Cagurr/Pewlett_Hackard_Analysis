125afdc8526994d38183b608a9a617fe10171186
Chris Gurr <cagurr.cg@gmail.com>
2022-07-24 14:51:24 -0400

    Adding work in progress @ 7.3.1

--- schema.sql
+++ schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE departments (
 CREATE TABLE employees (
     emp_no INT NOT NULL,
     birth_date DATE NOT NULL,
-    first_name VARCHAR (15) NOT NULL,
-    last_name VARCHAR (15) NOT NULL,
+    first_name VARCHAR (40) NOT NULL,
+    last_name VARCHAR (40) NOT NULL,
     gender VARCHAR (6) NOT NULL,
     hire_date DATE NOT NULL,
     PRIMARY KEY (emp_no)
@@ -31,25 +31,24 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+  PRIMARY KEY (emp_no)  
 );
 
 CREATE TABLE titles (
     emp_no INT NOT NULL,
-    title VARCHAR NOT NULL,
+    title VARCHAR (40) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    PRIMARY KEY (emp_no)
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE department_employees (
-    dept_no VARCHAR NOT NULL,
     emp_no INT NOT NULL,
+    dept_no VARCHAR (4) NOT NULL
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (dept_no, emp_no)
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
